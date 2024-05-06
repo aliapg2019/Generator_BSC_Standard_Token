8f7d2a432498e1bc443cab6dde046c54d958cce1
Ali Pashmgaran <apg.ali2019@gmail.com>
2024-05-07 01:32:56 +0330

    create standard BSC token

---
+++ contracts/token/BEP20/StandardBEP20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./lib/BEP20.sol";
+
+/**
+ * @title StandardBEP20
+ * @dev Implementation of the StandardBEP20
+ */
+contract StandardBEP20 is BEP20 {
+    
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 initialBalance
+    ) payable BEP20(name, symbol) {
+        require(initialBalance > 0, "StandardBEP20: supply cannot be zero");
+
+        _setupDecimals(decimals);
+        _mint(_msgSender(), initialBalance * 10 ** decimals);
+    }
+}
